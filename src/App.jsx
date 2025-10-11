88ded8c15e248592e48a1e37d163fed2d1331cfd
sebastian-manco-neostella <sebastian.manco@neostella.com>
2025-10-11 18:48:53 -0500

    feat(heading): styled components props and the "css" functions

--- src/App.jsx
+++ src/App.jsx
@@ -3,36 +3,7 @@ import styled, { ThemeProvider } from 'styled-components';
 import { GlobalStyles } from './styles/GlobalStyles';
 import Button from './ui/Button';
 import Input from './ui/inpust';
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-  background-color: tomato;
-`;
-
-// Define our button, but with the use of props.theme this time
-const ButtonTheme = styled.button`
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border-radius: 3px;
-
-  /* Color the border and text with theme.main */
-  color: ${props => props.theme.main};
-  border: 2px solid ${props => props.theme.main};
-`;
-
-// We are passing a default theme for Buttons that arent wrapped in the ThemeProvider
-ButtonTheme.defaultProps = {
-  theme: {
-    main: '#BF4F74',
-  },
-};
-
-// Define what props.theme will look like
-const theme = {
-  main: 'mediumseagreen',
-};
+import Heading from './ui/Heading';
 
 const StyledApp = styled.div`
   background-color: tomato;
@@ -41,25 +12,20 @@ const StyledApp = styled.div`
 `;
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <>
       <GlobalStyles />
       <StyledApp>
-        <H1>Hello from new skills</H1>
-        <H1>The Wild Oasis</H1>
+        <Heading as="h1">Hello from new skills</Heading>
+        <Heading as="h2">The Wild Oasis</Heading>
         <Button onClick={() => alert('You click me...')}>click me</Button>
         <Button onClick={() => alert('You click me...')}>click me 2</Button>
-        <Input type="text" placeholder="Here you could put your name." />
-
-        <div>
-          <Button>Default Theme</Button>
-
-          <ThemeProvider theme={theme}>
-            <ButtonTheme>Inverted Theme</ButtonTheme>
-          </ThemeProvider>
-        </div>
+        <Heading as="h3">Forms</Heading>
+        <di>
+          <Input type="text" placeholder="Here you could put your name." />
+          <Input type="text" placeholder="Here you could put your name." />
+          <Input type="text" placeholder="Here you could put your name." />
+        </di>
       </StyledApp>
     </>
   );
