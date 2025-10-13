3e3d27a08c9b7ee628a39b4952ed81da70c96fac
sebastian-manco-neostella <sebastian.manco@neostella.com>
2025-10-12 20:37:54 -0500

    feat(create-cabin-form): add form for add new cabins.
    create form component to handle new cabin and install react-hook-library

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,8 +1,13 @@
 import Heading from '../ui/Heading';
 import Row from '../ui/Row';
 import CabinTable from '../features/cabins/CabinTable';
+import { useState } from 'react';
+import Button from '../ui/Button';
+import CreateCabinForm from '../features/cabins/CreateCabinForm';
 
 function Cabins() {
+  const [showForm, setShowForm] = useState(false);
+
   // useEffect(function () {
   //   getCabins().then(data => console.log('data===>', data));
   // }, []);
@@ -16,6 +21,8 @@ function Cabins() {
 
       <Row>
         <CabinTable />
+        <Button onClick={() => setShowForm(show => !show)}>Add new cabin</Button>
+        {showForm && <CreateCabinForm />}
       </Row>
     </>
   );
